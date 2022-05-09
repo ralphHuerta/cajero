af68f2ad75e3c01b99a4c318b775e577d0356cfa
ralphHuerta <huerta.1996@hotmail.com>
2022-05-08 20:53:53 -0500

    Validar retirar

--- src/app/components/withdrawals/withdrawals.component.ts
+++ src/app/components/withdrawals/withdrawals.component.ts
@@ -39,14 +39,18 @@ export class WithdrawalsComponent implements OnInit {
     var cantidad=this.cantidadRetirar
     var sumaRetirar=0;
     var billetesValor= new Array()
-    var totalCajero= 0
-    
+    var totalCajero= this.total
+      console.log(this.total)
+      if(totalCajero< this.cantidadRetirar){
+        this.title= "No cuentas con suficiente efectivo"
+        this.primtfValores= null
+      }else{
       if(!auxNumber.includes('.') && (this.total >= this.cantidadRetirar && this.cantidadRetirar%50 == 0 )){
       
         if(this.noTranssacion){
             var i=0
             this.noTranssacion.forEach(function(element: any) {
-              totalCajero+= element.valor* element.cantidad
+             
               if(element.cantidad> 0){
                 if( (sumaRetirar+element.valor) <= cantidad){
                   while(((sumaRetirar+element.valor)<= cantidad) && (element.cantidad> 0)){
@@ -54,6 +58,7 @@ export class WithdrawalsComponent implements OnInit {
                     billetesValor[i]= element.valor
                     i++   
                     element.cantidad-=1
+                    totalCajero-=element.valor
                   }
                   
                 }
@@ -62,16 +67,13 @@ export class WithdrawalsComponent implements OnInit {
             }
           );
         }
-          if(totalCajero< this.cantidadRetirar){
-            this.title= "No cuentas con suficiente efectivo"
-            this.primtfValores= null
-
-          }else{
             if(sumaRetirar== this.cantidadRetirar){
               this.title= "Transacción exitosa"
               //this.denominaciones= noTranssacion
               this.primtfValores= billetesValor
-              this.total = totalCajero
+              this.total=totalCajero
+              console.log(this.total)
+
             // this.FinalDenominaciones.emit(this.denominaciones)
             }else{
               this.title ="No se puede hacer esa transacción, por favor ingrese otra cantidad"
@@ -79,12 +81,12 @@ export class WithdrawalsComponent implements OnInit {
               this.primtfValores= null
               
             }
-          }
         
       }else{
         this.title ="No se puede hacer esa transacción, por favor ingrese otra cantidad"
               
       }
+    }
 
   }
 
